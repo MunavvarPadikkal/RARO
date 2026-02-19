32ff923bb82238e7b47f0d1416a787108e06e848
munu <pkmunavar@gmail.com>
2026-02-19 20:30:29 +0530

    Initial commit

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 const express = require("express");
 const app = express();
-const env = require("dotenv").config();
+const path = require("path");
+const env = require("dotenv")
 const db = require("./config/db");
+const userRouter = require("./routes/userRouter");
+
 db();
 
+env.config();
+const PORT = process.env.PORT || 3000
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.set("view engine","ejs");
+app.set("views",[path.join(__dirname,"views/user"),path.join(__dirname,"views/admin")]);
+app.use(express.static(path.join(__dirname, "public")));
 
 
-app.listen(process.env.PORT, ()=>{
-    console.log("Server is Running");
+app.use("/",userRouter)
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running at: http://localhost:${PORT}`);
 });
 
 
---
+++ controllers/user/userController.js
@@ -0,0 +1,26 @@
+
+const pageNotFound = async(req, res)=>{
+    try {
+        res.render("pageNotFound")
+    } catch (error) {
+        res.redirect("/pageNotFound")
+    }
+}
+
+
+const loadHomepage = async (req,res)=>{
+    try{
+        
+        res.render("home");
+
+    }catch (error){
+        console.log("Home page not found");
+        res.status(500).send("server error");
+    }
+}
+
+
+
+module.exports = {
+    loadHomepage, pageNotFound
+}
